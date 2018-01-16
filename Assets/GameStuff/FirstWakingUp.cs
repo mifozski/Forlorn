b1d0fafc0086938c5ed407b4c1ef797eddf8686a
Andrey Efimov <mifozski@gmail.com>
2018-01-16 23:33:30 +0300

    Improrvment of all scenes. Started on game stages and messaging system

---
+++ Assets/GameStuff/FirstWakingUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DTAnimatorStateMachine;
+
+public class FirstWakingUp : DTStateMachineBehaviour<int> {
+
+	void OnInitialized()
+	{
+	}
+
+	void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		Debug.Log("FirstWakingUp::OnStateEntered()");
+		AudioSource knockingSound = GameObject.Find("KnockingSound").GetComponent<AudioSource>();
+		knockingSound.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Room/CityLightsMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityLightsMover : MonoBehaviour {
+
+	Transform camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(camera.position.x, transform.position.y, transform.position.z);
+	}
+}

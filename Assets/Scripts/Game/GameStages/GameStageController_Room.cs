329742847e61e25a65a49e906b2a22528571bea8
Andrey Efimov <mifozski@gmail.com>
2018-10-28 23:38:45 +0300

    New materials/textures

---
+++ Assets/Scripts/Game/GameStages/GameStageController_Room.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Forlorn
+{
+	public class GameStageController_Room : MonoBehaviour
+	{
+		void Awake()
+		{
+
+		}
+
+		void Start ()
+		{
+
+		}
+
+		void Update ()
+		{
+
+		}
+	}
+}

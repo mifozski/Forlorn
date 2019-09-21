cf8010203a73cc1ec5ade41b9481cfa91ea5ef10
Andrey Efimov <aefimov@evernote.com>
2019-09-21 23:24:43 +0300

    reverb zone mixin

---
+++ Assets/Scripts/Mixins/ReverbZoneMixin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ReverbZoneMixin : MonoBehaviour
+{
+	[SerializeField] AudioReverbZone reverbZone;
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			reverbZone.enabled = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			reverbZone.enabled = false;
+		}
+	}
+}
